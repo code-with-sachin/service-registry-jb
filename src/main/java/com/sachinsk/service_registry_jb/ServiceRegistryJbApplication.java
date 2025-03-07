e4ce7c75c1cec5070a1dcfc19809ba909d4c5369
Sachin <sachinkaushikofficial@gmail.com>
2025-03-07 13:08:38 +0530

    Adding eureka configuration

--- src/main/java/com/sachinsk/service_registry_jb/ServiceRegistryJbApplication.java
+++ src/main/java/com/sachinsk/service_registry_jb/ServiceRegistryJbApplication.java
@@ -2,8 +2,10 @@ package com.sachinsk.service_registry_jb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistryJbApplication {
 
 	public static void main(String[] args) {
