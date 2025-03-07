fbec9afd1b757744096c390c5bb49cace59aa63b
Sachin <sachinkaushikofficial@gmail.com>
2025-03-07 13:02:47 +0530

    Initial commit

---
+++ src/main/java/com/sachinsk/service_registry_jb/ServiceRegistryJbApplication.java
@@ -0,0 +1,13 @@
+package com.sachinsk.service_registry_jb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistryJbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryJbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sachinsk/service_registry_jb/ServiceRegistryJbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachinsk.service_registry_jb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryJbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
